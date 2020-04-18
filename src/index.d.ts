f9773d998c4f76d7da2c92cc7f53f737f821185a
Viviane <vivianedias@protonmail.com>
2020-04-18 20:02:33 -0300

    feat(imports): export new components

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,8 @@
 export { default as Mobilization } from './components/Mobilization';
 export { default as Footer } from './components/Footer';
 export { default as Section } from './components/Section';
-export { default as Navigation } from './components/Navigation';
+export { default as Navigation } from './components/navigation';
 export { default as WidgetArea } from './components/WidgetArea';
-export { default as PluggableWidget } from './components/UX/PluggableWidget';
+export { default as PluggableWidget } from './components/ux/PluggableWidget';
+export { default as FormPlugin } from './plugins/Form';
+export { default as asyncFormEntryCreate } from './redux/action-creators';
--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,6 @@ export { default as Section } from './components/Section';
 export { default as Navigation } from './components/navigation';
 export { default as WidgetArea } from './components/WidgetArea';
 export { default as PluggableWidget } from './components/ux/PluggableWidget';
+export { default as FormPlugin } from './plugins/Form/Plugin';
+export { default as selectors } from './redux/Selectors';
+export { default as asyncFormEntryCreate } from './redux/action-creators/FormEntryCreate';
