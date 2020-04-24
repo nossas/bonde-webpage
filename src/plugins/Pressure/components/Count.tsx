de2aa71e8f2feb44740e1952e47329c8ffe75499
Viviane <vivianedias@protonmail.com>
2020-04-24 18:04:49 -0300

    feat(count): styles pressure count component

--- src/plugins/Pressure/components/Count.tsx
+++ src/plugins/Pressure/components/Count.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import CountUp from 'react-countup';
+import styled from 'styled-components';
+import theme from '../../../base/theme';
 
-const styles = {
-  count: {
-    boxShadow: 'inset 0px 15px 18px -10px rgba(227,224,227,1)',
-    WebkitBoxShadow: 'inset 0px 15px 18px -10px rgba(227,224,227,1)',
-    MozBoxShadow: 'inset 0px 15px 18px -10px rgba(227,224,227,1)',
-  },
+const Wrapper = styled.div<{ theme: { fontFamily: string } }>`
+  font-family: ${props => props.theme.fontFamily};
+  box-shadow: rgb(227, 224, 227) 0px 15px 18px -10px inset;
+  background-color: #fff;
+  padding: 2rem;
+  border-radius: 0 0 3px 3px;
+  display: grid;
+  justify-items: center;
+  & > div {
+    transition: left 0.5s ease;
+  }
+`;
+
+Wrapper.defaultProps = {
+  theme,
 };
+
+const Title = styled.h1<{ color: string }>`
+  color: ${props => props.color};
+  font-weight: 300;
+  font-size: 2.15rem;
+  margin: 15px 0;
+`;
+
+const Subtitle = styled.h3`
+  color: #111;
+  font-weight: 700;
+  font-size: 1.25rem;
+  margin: 0;
+`;
 
 type Props = {
   value: number;
@@ -18,27 +43,23 @@ type Props = {
 
 const Count = ({ value, color, text }: Props) => {
   return (
-    <div
-      className="pressure-count p3 bg-white rounded-bottom"
-      style={styles.count}
-    >
-      <div className="center m0">
-        <div className="h1" style={{ color }}>
-          <CountUp
-            start={0}
-            end={!isNaN(value) ? Number(value) : 0}
-            duration={5}
-          />
-        </div>
-        <span className="black bold h3 ml1">{text || 'pressões feitas'}</span>
-      </div>
-    </div>
+    <Wrapper>
+      <Title color={color}>
+        <CountUp
+          start={0}
+          end={!isNaN(value) ? Number(value) : 0}
+          duration={5}
+        />
+      </Title>
+      <Subtitle>{text || 'pressões feitas'}</Subtitle>
+    </Wrapper>
   );
 };
 
 Count.defaultProps = {
   value: 0,
   startCounting: false,
+  theme,
 };
 
 export default Count;
--- src/plugins/Pressure/components/index.ts
+++ src/plugins/Pressure/components/index.ts
@@ -1,3 +1,4 @@
-export { default as Targets } from './Targets/TargetList';
+export { default as Targets } from './Targets';
 export { default as Count } from './Count';
 export { default as TellAFriend } from './TellAFriend';
+export { default as Form } from './Form';
