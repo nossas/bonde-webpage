b7b5106f702c2c112647cc8ecf6ff525c5868902
Viviane <vivianedias@protonmail.com>
2020-04-28 13:17:36 -0300

    improvement(tests): fixes test watch script by using tsdx runner and add one more test to button

--- src/plugins/Form/__tests__/Button.spec.tsx
+++ src/plugins/Form/__tests__/Button.spec.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, shallow } from 'enzyme';
 import { Button } from '../components';
 
-describe('A suite', function() {
+describe('Form Button', function() {
   const buttonText = 'Enviar';
+
+  it('should render button', () => {
+    const wrapper = shallow(<Button buttonText={buttonText} />);
+
+    expect(wrapper.exists()).toBeTruthy();
+  });
 
   it('should render to static HTML', function() {
     expect(render(<Button buttonText={buttonText} />).text()).toEqual('Enviar');
