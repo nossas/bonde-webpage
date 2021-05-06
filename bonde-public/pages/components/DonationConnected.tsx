fa126384d519abaee1118348c7f1bed3deafcb81
Igor Santos <igr.exe@gmail.com>
2021-05-06 16:46:36 -0300

    fix: donation request ssr

--- bonde-public/pages/components/DonationConnected.tsx
+++ bonde-public/pages/components/DonationConnected.tsx
@@ -2,7 +2,6 @@
 import {
   DonationPlugin,
   PagarMeCheckout,
-  asyncDonationCreate,
   asyncDonationConvert,
   FinishMessageCustom,
   FinishDonationMessage,
@@ -10,17 +9,18 @@ import {
   DonationTellAFriend
 } from 'bonde-webpages';
 import { connect } from 'react-redux';
-// import { client } from '../../graphql-app';
 import getConfig from 'next/config';
 import Utils from '../../Utils';
 import fetch from 'node-fetch';
 
 const { publicRuntimeConfig } = getConfig();
 
-// const mapStateToProps = () => ({ client });
-
 const mapDispatchToProps = () => ({
-  createTransaction: asyncDonationCreate,
+  createTransaction: async (args: any) => (await fetch('/api/actions/donation', {
+    method: 'post',
+    body: JSON.stringify(args),
+    headers: { 'Content-Type': 'application/json' }
+  })).json(),
   asyncDonationConvert,
   asyncFetchDonationsStats: async (args: any) => (await fetch('/api/data/donations', {
     method: 'post',
