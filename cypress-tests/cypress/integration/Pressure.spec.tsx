cd0c0075db381cab8c6357278d1e445fa35315d1
Adriele A Secundino <dri.leela@gmail.com>
2021-07-02 13:46:37 -0300

    chore:add test fill form without name

--- cypress-tests/cypress/integration/Pressure.spec.tsx
+++ cypress-tests/cypress/integration/Pressure.spec.tsx
@@ -1,20 +1,40 @@
 describe('Make Pressure', () => {
     beforeEach(() => {
         cy.visit('http://www.teste-de-widgets.staging.bonde.org/')
-    })
+    });
     it('Fill pressure form', () => {
         cy.get('[name ="name"]')
-            .should('be.visible').type('Nome')
+            .should('be.visible').type('Nome');
         cy.get('[name ="email"]').should('be.visible')
-            .type('email@exemplo.com')
+            .type('email@exemplo.com');
         cy.get('[name ="lastname"]')
-            .should('be.visible').type('Sobrenome')
+            .should('be.visible').type('Sobrenome');
         cy.get('[name ="city"]')
-            .should('be.visible').type('Cidade')
+            .should('be.visible').type('Cidade');
+        
+        //send email button    
         cy.get('.sc-AxheI')
             .should('be.visible')
-            .click()
+            .click();
+        
+        //confirmation    
         cy.get('.css-aomufq > img')
-            .should('be.visible')
-    })
-})
+            .should('be.visible');
+    });
+
+    it('incomplete fill form without name', () => { 
+        cy.get('[name ="email"]') .should('be.visible') 
+        .type('email@exemplo.com'); 
+        cy.get('[name ="lastname"]') 
+        .should('be.visible') .type('Sobrenome'); 
+        cy.get('[name ="city"]') 
+        .should('be.visible') .type('Cidade') ;
+        cy.get('.sc-AxheI') 
+        .should('be.visible') 
+        .click();
+
+        //required field 
+        cy.get('.sc-fzpjYC') 
+        .should('be.visible');    
+    });
+});
