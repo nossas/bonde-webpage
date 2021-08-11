929431f43316ccfc723ec486d2f389789038c73e
Igor Santos <igr.exe@gmail.com>
2021-08-11 15:17:14 -0300

    feat(public): change pressure text

--- bonde-public/initialI18nStore/locales/es.ts
+++ bonde-public/initialI18nStore/locales/es.ts
@@ -6,7 +6,7 @@ export default {
   "Disclaimer": "Al ingresar sus datos, acepta que sus datos se compartan con los organizadores de esta página y acepta recibir correos electrónicos actualizados, como se describe en la <1>política de privacidad</1>. Puede optar por no recibir estos correos electrónicos en cualquier momento.",
   "Pressure Target Label": "¿A quién vas a presionar? ({{count}} objetivos)",
   "Default Post Action": "Comparta la campaña con más personas para aumentar el impacto.",
-  "Pressure Post Action": "¡Correos electrónicos enviados!",
+  "Pressure Post Action": "¡Se enviarán correos electrónicos!",
   "Form Post Action": "¡Formulario guardado!",
   "Form Blank Validation": "{{field}} no puede estar en blanco",
   "Pressure Blank Validation": "Llenado obligatorio",
