45c41450bb2fe1fd65a6d93882f2470613c3a503
Adriele A Secundino <dri.leela@gmail.com>
2021-08-16 17:35:38 -0300

    chore: add option to indicate the number of times that the test will run

--- cypress-tests/cypress/integration/DonationForm.spec.tsx
+++ cypress-tests/cypress/integration/DonationForm.spec.tsx
@@ -2,14 +2,17 @@ describe('Make Donation', () => {
     beforeEach(() => {
         cy.visit('/')
     });
-    it('Fill donation form',async () => {
-        cy.get('.css-1aupfcx')
-            .should('be.visible');
-       cy.get('.btn-submit')
-            .should('be.visible')
-            .click()
-        //TODO - fazer a confirmação do checkout para o PagarMe       
-        cy.get('[id="pagarme-checkout-container"]')
-            .should('exist');   
+    
+    Cypress._.times(1,() => {
+        it('Fill donation form',async () => {
+            cy.get('.css-1aupfcx')
+                .should('be.visible');
+            cy.get('.btn-submit')
+                .should('be.visible')
+                .click()
+            //TODO - fazer a confirmação do checkout para o PagarMe       
+            cy.get('[id="pagarme-checkout-container"]')
+                .should('exist');   
+        });
     });
 });
