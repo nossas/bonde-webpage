f2879e7f66a9f8e5a36294f32a19db9df2558f95
Adriele A Secundino <dri.leela@gmail.com>
2021-08-16 17:41:59 -0300

    chore: updated pressure test to add number of times to run, change identifier of the send button and add state field

--- cypress-tests/cypress/integration/Pressure.spec.tsx
+++ cypress-tests/cypress/integration/Pressure.spec.tsx
@@ -2,39 +2,46 @@ describe('Make Pressure', () => {
     beforeEach(() => {
         cy.visit('/')
     });
-    it('Fill pressure form', () => {
-        cy.get('[name ="name"]')
-            .should('be.visible').type('Nome');
-        cy.get('[name ="email"]').should('be.visible')
-            .type('email@exemplo.com');
-        cy.get('[name ="lastname"]')
-            .should('be.visible').type('Sobrenome');
-        cy.get('[name ="city"]')
-            .should('be.visible').type('Cidade');
+    
+    Cypress._.times(1,(i) => { 
+        it('Fill pressure form', () => {
+            cy.get('[name ="name"]')
+                .should('be.visible').type('Nome');
+            cy.get('[name ="email"]').should('be.visible')
+                .type(i + 'email@exemplo.com');
+            cy.get('[name ="lastname"]')
+                .should('be.visible').type('Sobrenome');
+            cy.get('[name ="city"]')
+                .should('be.visible').type('Cidade');
+            cy.get('[name ="state"]')
+                .should('be.visible').select('MG');
+
+            //send email button    
+            cy.get('.sc-fzqBZW')
+                .should('be.visible')
+                .click();
         
-        //send email button    
-        cy.get('.sc-AxheI')
-            .should('be.visible')
-            .click();
-        
-        //confirmation    
-        cy.get('.css-aomufq > img')
-            .should('be.visible');
+            //confirmation    
+            cy.get('.css-aomufq > img')
+                .should('be.visible');
+        });
     });
 
     it('incomplete fill form without name', () => { 
         cy.get('[name ="email"]') .should('be.visible') 
-        .type('email@exemplo.com'); 
+            .type('email@exemplo.com'); 
         cy.get('[name ="lastname"]') 
-        .should('be.visible') .type('Sobrenome'); 
+            .should('be.visible') .type('Sobrenome'); 
         cy.get('[name ="city"]') 
-        .should('be.visible') .type('Cidade') ;
-        cy.get('.sc-AxheI') 
-        .should('be.visible') 
-        .click();
+            .should('be.visible') .type('Cidade') ;
+        cy.get('[name ="state"]')
+            .should('be.visible').select('MG');
+        cy.get('.sc-fzqBZW') 
+            .should('be.visible') 
+            .click();
 
         //required field 
-        cy.get('.sc-fzpjYC') 
-        .should('be.visible');    
+        cy.get(':nth-child(3) > .sc-fzqPZZ > .sc-fzqBkg')
+            .should('be.visible');    
     });
 });
