31eb555a15e804b6b51452452421efb2375ab25f
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-09-14 15:55:46 -0300

    fix: update Pressure test

--- cypress-tests/cypress/integration/Pressure.spec.tsx
+++ cypress-tests/cypress/integration/Pressure.spec.tsx
@@ -1,47 +1,47 @@
 describe('Make Pressure', () => {
-    beforeEach(() => {
-        cy.visit('/')
-    });
-    
-    Cypress._.times(1,(i) => { 
-        it('Fill pressure form', () => {
-            cy.get('[name ="name"]')
-                .should('be.visible').type('Nome');
-            cy.get('[name ="email"]').should('be.visible')
-                .type(i + 'email@exemplo.com');
-            cy.get('[name ="lastname"]')
-                .should('be.visible').type('Sobrenome');
-            cy.get('[name ="city"]')
-                .should('be.visible').type('Cidade');
-            cy.get('[name ="state"]')
-                .should('be.visible').select('MG');
+  beforeEach(() => {
+    cy.visit('/')
+  });
 
-            //send email button    
-            cy.get('.sc-fzqBZW')
-                .should('be.visible')
-                .click();
-        
-            //confirmation    
-            cy.get('.css-aomufq > img')
-                .should('be.visible');
-        });
-    });
+  Cypress._.times(1, (i) => {
+    it('Fill pressure form', () => {
+      cy.get('[name ="email"]').eq(0).
+        should('be.visible').type('email@exemplo.com');
+      cy.get('[name="name"]').eq(0)
+        .should('be.visible').type('Nome');
+      cy.get('[name ="lastname"]').eq(0)
+        .should('be.visible').type('Sobrenome');
+      cy.get('[name ="city"]').eq(0)
+        .should('be.visible').type('Cidade');
+      cy.get('[name ="state"]').eq(0)
+        .should('be.visible').select('MG');
 
-    it('incomplete fill form without name', () => { 
-        cy.get('[name ="email"]') .should('be.visible') 
-            .type('email@exemplo.com'); 
-        cy.get('[name ="lastname"]') 
-            .should('be.visible') .type('Sobrenome'); 
-        cy.get('[name ="city"]') 
-            .should('be.visible') .type('Cidade') ;
-        cy.get('[name ="state"]')
-            .should('be.visible').select('MG');
-        cy.get('.sc-fzqBZW') 
-            .should('be.visible') 
-            .click();
+      //send email button    
+      cy.get('.sc-fzqBZW').eq(0)
+        .should('be.visible')
+        .click();
 
-        //required field 
-        cy.get(':nth-child(3) > .sc-fzqPZZ > .sc-fzqBkg')
-            .should('be.visible');    
+      //confirmation
+      cy.get('.css-aomufq > img')
+        .should('be.visible');
     });
+  });
+
+  it('incomplete fill form without name', () => {
+    cy.get('[name ="email"]').eq(0).should('be.visible')
+      .type('email@exemplo.com');
+    cy.get('[name ="lastname"]').eq(0)
+      .should('be.visible').type('Sobrenome');
+    cy.get('[name ="city"]').eq(0)
+      .should('be.visible').type('Cidade');
+    cy.get('[name ="state"]').eq(0)
+      .should('be.visible').select('MG');
+    cy.get('.sc-fzqBZW').eq(0)
+      .should('be.visible')
+      .click();
+
+    //required field 
+    cy.get('[class="sc-fzqBkg duaeRa"]').eq(0)
+      .should('be.visible');
+  });
 });
